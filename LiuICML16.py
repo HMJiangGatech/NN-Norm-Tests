a8f50a38b1feff755fa65f5015b92f83798075a7
Haoming Jiang <jianghm.ustc@gmail.com>
2017-09-18 10:22:24 -0400

    18/09/17

--- LiuICML16.py
+++ LiuICML16.py
@@ -122,10 +122,11 @@ def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
 
+#def Lsoftmax_loss()
 
-def main(_):
+def main():
   # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+  mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
   # Create the model
   x = tf.placeholder(tf.float32, [None, 784])
@@ -168,9 +169,4 @@ def main(_):
         x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str,
-                      default='MNIST_data',
-                      help='Directory for storing input data')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  main()
